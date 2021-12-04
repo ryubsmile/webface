f3d570f1629091d5999544b55a7870c066aa3db7
ryubsmile <66876469+ryubsmile@users.noreply.github.com>
2021-12-05 02:30:49 +0900

    added warning message in about

--- src/components/articles/pages/About/About.tsx
+++ src/components/articles/pages/About/About.tsx
@@ -15,8 +15,11 @@ const aboutDescription = (
   <>
     Project WebFace is my frontend capstone portfolio, built via the Github Pages. <br />
     WebFace is where I upload all my visual creations made throughout my high school years. <br />
-    The main tech stacks include: HTML, CSS, Typescript and React. <br />
-    It is continuously getting updated, so please bookmark this page if you are interested. <br />
+    The main tech stacks include: HTML, CSS, Typescript, and React. <br />
+    WebFace itself is also a study that I am working on continuously. <br /> <br />
+    It is getting updated regularly, and in the middle of it there might be bugs and glitches in the website 
+    -- especially in phones and other devices -- so if you find them, <br />
+    please report to me by referring to the Contacts page. You can bookmark this page if this interests you! <br />
     Navigate to the other tabs to read more. Thank you!
   </>
 );
