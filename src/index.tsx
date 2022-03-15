28037f85cc041d072a002cdd1ec9df5b01edcb02
ryubsmile <66876469+ryubsmile@users.noreply.github.com>
2022-03-15 23:08:48 +0900

    background to grey, minor font updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-/** 
+/**
  * Main, and topmost javascript file that shows all the components according to order.
- * 
- * If a component is somehow not connected to this script, 
+ *
+ * If a component is somehow not connected to this script,
  * it will not show up on the website no matter what.
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TopLevelContent from './components/TopLevelContent';
+import './index.css';
 
-const PageRouter = () => { 
-  const defaultURL = "/webface";
-  const pathName: string = window.location.pathname + window.location.hash; 
+const PageRouter = () => {
+  const defaultURL = '/webface';
+  const pathName: string = window.location.pathname + window.location.hash;
 
   return <TopLevelContent defaultURL={defaultURL} entireURL={pathName} />;
 };
